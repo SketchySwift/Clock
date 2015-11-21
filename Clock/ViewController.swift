2d7b9229397af9a392cf417d84f5419b7313a1c7
HardcoreIOS <Abdurrahman@owners-mbp.home>
2015-11-20 20:34:17 -0500

    This Is My Clock App!

--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateTime() {
+        timeLabel.text = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: NSDateFormatterStyle.MediumStyle/* <here as well */, timeStyle: NSDateFormatterStyle.MediumStyle/* <here change to longStyle, mediumStyle, fullStyle, etc... */)
     }
 
-
+    // NOTES:
+    // Thank you for downloading this project!
+    // You don't need to change any code to get your local time! By Xcode, it gets your local time just by writing a few lines of code above!
+    // However, you could change the format you want. Play with it! The ones with arrows that I put next to!
+    // Change the design and colors around if you like! But anyways, once again thank you and have fun!😀
 }
 
